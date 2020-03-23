2138a71a0807aac16732a485406333a644773d53
Shane6712 <53222653+Shane6712@users.noreply.github.com>
2020-03-23 14:01:50 -0400

    Update homedir.bat

--- ProfileUnity/Scripts/homedir.bat
+++ ProfileUnity/Scripts/homedir.bat
@@ -1,1 +1,14 @@
+@echo off
+REM ********************Liquidware ProfileUnity Config Managment*************************
+
+if not exist \\vdi.com\data\home\%username%\MyIni md \\vdi.com\data\home\%username%\MyIni
+if not exist \\vdi.com\data\home\%username%\MyIni\profileunity.lic echo f | xcopy \\vdi.com\NETLOGON\ProfileUnity\Configs\User\profileunity.lic \\vdi.com\data\home\%username%\MyIni\profileunity.lic| xcopy \\vdi.com\data\home\%username%\MyIni\profileunity.lic
+
+
+if not exist \\vdi.com\data\home\%username%\MyIni\*.ini goto exit
+xcopy /y \\vdi.com\NETLOGON\ProfileUnity\Configs\User\*.ini \\vdi.com\data\home\%username%\MyIni
+reg add "HKCU\Software\Liquidware Labs\ProfileUnity" /v INIPath /t REG_SZ /d \\vdi.com\data\home\%username%\MyIni /f
+
+:exit
+exit
 
